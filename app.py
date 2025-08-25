62c88f586b1bb424a81b10ea0820c2470e0c72be
Vinh Nguyen <thicckhq@gmail.com>
2025-08-25 15:00:36 +0700

    Initial commit for Render

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Chào mừng bạn đến với ứng dụng Python trên Heroku!</h1>'
+    return '<h1>Chào mừng bạn đến với Render!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
