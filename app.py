522cf7c3c50be012ca1df5ed22ddaa315be54f30
Vinh Nguyen <thicckhq@gmail.com>
2025-08-26 19:57:38 +0700

    Updated html

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ engine = create_engine(DATABASE_URL, echo=False)
 def ping_server():
     while True:
         try:
-            url = os.getenv("PING_URL", "https://your-app.onrender.com")
+            url = os.getenv("PING_URL", "https://tracnghiemhq.onrender.com")
             requests.get(url)
         except Exception as e:
             print("Ping error:", e)
@@ -85,24 +85,13 @@ def register():
         return redirect(url_for('login'))
 
     with engine.begin() as conn:
-        # Kiểm tra sự tồn tại của username
-        exist_username = conn.execute(
-            text("SELECT 1 FROM Nguoidung WHERE username=:u"),
-            {"u": username}
+        exist = conn.execute(
+            text("SELECT 1 FROM Nguoidung WHERE username=:u OR email=:e"),
+            {"u": username, "e": email}
         ).first()
-        if exist_username:
-            flash("Tên đăng nhập đã tồn tại!")
+        if exist:
+            flash("Tên đăng nhập hoặc Email đã tồn tại!")
             return redirect(url_for('login'))
-            
-        # Kiểm tra sự tồn tại của email (nếu email được cung cấp)
-        if email:
-            exist_email = conn.execute(
-                text("SELECT 1 FROM Nguoidung WHERE email=:e"),
-                {"e": email}
-            ).first()
-            if exist_email:
-                flash("Email đã tồn tại!")
-                return redirect(url_for('login'))
 
         pw_hash = generate_password_hash(password)
         conn.execute(text("""
